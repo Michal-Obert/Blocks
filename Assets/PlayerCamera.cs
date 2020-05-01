60a1f6b9144e27c6c39fdc9c6b81df3fc398bf79
Michal-Obert <Michal-Obert@users.noreply.github.com>
2020-05-01 16:24:23 +0200

    +Added player capable of simple movement

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+	// CONFIGURATION
+
+	[SerializeField]
+	private Transform m_PlayerTransform;
+
+	// PRIVATE PROPERTIES
+
+	private Transform m_Transform;
+	private float     m_MouseDirX;
+	private float     m_MouseDirY;
+
+	// MONO OVERRIDES
+
+	private void Awake()
+	{
+		m_Transform      = transform;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update()
+	{
+		m_MouseDirX                    += Input.GetAxis("Mouse X");
+		m_MouseDirY                    += Input.GetAxis("Mouse Y");
+		m_MouseDirY                     = Mathf.Clamp(m_MouseDirY, -60f, 60f);
+		m_Transform.localRotation       = Quaternion.AngleAxis(-m_MouseDirY, Vector3.right);
+		m_PlayerTransform.localRotation = Quaternion.AngleAxis(m_MouseDirX, m_PlayerTransform.up);
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	//CONFIGURATION
+
+	[SerializeField]
+	private CharacterController m_CharacterController;
+	[SerializeField]
+	private float               m_Speed;
+	[SerializeField]
+	private float               m_JumpHeight;
+
+	//PRIVATE PROPERTIES
+
+	private Transform m_Transform;
+	private float     m_Gravity             = -4f;
+	private Vector3   m_RemainingVelocity;
+
+	// MONO OVERRIDES
+
+	private void Awake()
+	{
+		m_Transform = transform;
+	}
+
+	void Update()
+	{
+		var deltaTime          = Time.deltaTime;
+		var horizontalMovement = Input.GetAxis("Horizontal");
+		var verticalMovement   = Input.GetAxis("Vertical");
+		var movement           = Vector3.zero;
+
+		if (m_CharacterController.isGrounded)
+		{
+			movement  = m_Transform.forward * verticalMovement + m_Transform.right * horizontalMovement;
+			movement.Normalize();
+			movement *= m_Speed;
+			
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				m_RemainingVelocity   = movement;
+				m_RemainingVelocity.y = Mathf.Sqrt(m_JumpHeight * -2f * m_Gravity);
+			}
+			else
+			{
+				m_RemainingVelocity = Vector3.zero;
+			}
+		}
+		else
+		{
+			movement.y += m_Gravity;
+		}
+		m_RemainingVelocity.y += m_Gravity * deltaTime;
+
+		m_CharacterController.Move((movement + m_RemainingVelocity) * deltaTime);
+	}
+}
