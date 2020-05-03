7e96b54bf51bc0b964537dccf86c51078a18dbb6
Michal-Obert <Michal-Obert@users.noreply.github.com>
2020-05-03 08:54:22 +0200

    +TestScene with DebugData for easier testing

---
+++ Assets/DebugData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class DebugData : MonoBehaviour
+{
+	// CONFIGURATION
+
+	[SerializeField]
+	private UnityEngine.UI.Text m_DebugText;
+
+	// PRIVATE PROPERTIES
+
+	private float m_FPSCheckTimer;
+	private Game  m_Game;
+
+	// MONO OVERRIDES
+
+	void Start()
+	{
+		m_Game = GameObject.Find("Game").GetComponent<Game>();
+	}
+
+	void Update()
+	{
+		m_FPSCheckTimer += Time.deltaTime;
+
+		if (m_FPSCheckTimer > 0.5f) //no point allocating every frame
+		{
+			m_DebugText.text = $"FPS: {(int)(1 / Time.unscaledDeltaTime)}\n" +
+				               $"CurrentChunk: {m_Game.PlayerChunkPosition.ToString("0")}\n" +
+				               $"LookChunk:    {m_Game.PlayerLookChunk.ToString("0")}";
+			m_FPSCheckTimer  = 0;
+		}
+	}
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -9,9 +9,13 @@ public class Game : MonoBehaviour
 	[SerializeField]
 	private GameObject m_CubePrefab;
 
+	// PUBLIC PROPERTIES
+
+	public  Vector2      PlayerChunkPosition { get; private set; }
+	public  Vector2      PlayerLookChunk     { get { return -Vector2.one; } }
+
 	// PRIVATE PROPERTIES
 
-	private Vector2      m_PlayerChunkPosition;
 	private Transform    m_Player;
 	private ChunkManager m_ChunkManager;
 
@@ -19,15 +23,15 @@ public class Game : MonoBehaviour
 
 	void Start()
 	{
-		m_ChunkManager        = new ChunkManager(m_CubePrefab);
+		m_ChunkManager      = new ChunkManager(m_CubePrefab);
 		m_ChunkManager.GenerateInitialWorld();
 
-		m_Player              = GameObject.Instantiate(m_PlayerPrefab).transform;
-		m_Player.position     = new Vector3(0, Chunk.SIZE, 0); ;
-		m_PlayerChunkPosition = new Vector2(0, 0);
+		m_Player            = GameObject.Instantiate(m_PlayerPrefab).transform;
+		m_Player.position   = new Vector3(0, Chunk.SIZE, 0); ;
+		PlayerChunkPosition = new Vector2(0, 0);
 	}
 
-	private void Update()
+	void Update()
 	{
 		var playerPos    = m_Player.position;
 		if (playerPos.x < 0)
@@ -39,10 +43,10 @@ public class Game : MonoBehaviour
 		var playerChunkY = (int)playerPos.z / Chunk.SIZE;
 		var newChunkPos  = new Vector2(playerChunkX, playerChunkY);
 
-		if (newChunkPos != m_PlayerChunkPosition)
+		if (newChunkPos != PlayerChunkPosition)
 		{
-			m_ChunkManager.WorldResize(m_PlayerChunkPosition, newChunkPos);
-			m_PlayerChunkPosition = newChunkPos;
+			m_ChunkManager.WorldResize(PlayerChunkPosition, newChunkPos);
+			PlayerChunkPosition = newChunkPos;
 		}
 	}
 }
