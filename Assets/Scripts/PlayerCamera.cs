ccf278039319814684b2bc45d71b0277fdcae91e
Michal-Obert <Michal-Obert@users.noreply.github.com>
2020-05-03 22:03:00 +0200

    *Camera behaviour improved
    Player can now easier dig up/down and camera is no longer clipping walls of near cubes

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -25,7 +25,7 @@ public class PlayerCamera : MonoBehaviour
 	{
 		m_MouseDirX                    += Input.GetAxis("Mouse X");
 		m_MouseDirY                    += Input.GetAxis("Mouse Y");
-		m_MouseDirY                     = Mathf.Clamp(m_MouseDirY, -60f, 60f);
+		m_MouseDirY                     = Mathf.Clamp(m_MouseDirY, -80f, 75f);
 		m_Transform.localRotation       = Quaternion.AngleAxis(-m_MouseDirY, Vector3.right);
 		m_PlayerTransform.localRotation = Quaternion.AngleAxis(m_MouseDirX, m_PlayerTransform.up);
 	}
