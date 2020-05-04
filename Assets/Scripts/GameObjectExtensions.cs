69e2a2f0cbbee166bf2ef4686ceb4cce69ac6af2
Michal-Obert <Michal-Obert@users.noreply.github.com>
2020-05-04 06:50:46 +0200

    Optimized version of GameObject.SetActive
    Unity does not check if gameObject is already (in)active before calling SetActive to C++ code, creating unnecessary overhead. SetActiveSafe solves this

---
+++ Assets/Scripts/GameObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class GameObjectExtensions
+{
+	public static void SetActiveSafe(this GameObject gameObject, bool active)
+	{
+		if (gameObject.activeSelf != active)
+			gameObject.SetActive(active);
+	}
+}
